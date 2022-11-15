b524c80bd4bdadc1761f80b32a1c53900c2b96c9
DashCruft <theporyafm12@gmail.com>
2022-11-14 16:52:52 -0800

    Updated Version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,41 +1,47 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
 use std::env;
+use tauri::Manager;
 
+#[cfg(target_os = "windows")]
 fn main() {
-  if cfg!(windows) {
-      tauri::Builder::default()
-          .invoke_handler(tauri::generate_handler![get_input, show_main_window])
-          .setup(|app| {
-              let window = app.get_window(&"main").unwrap();
-              window_shadows::set_shadow(&window, true).expect("Unsupported platform!");
-              Ok(())
-          })
-          .run(tauri::generate_context!())
-          .expect("error while running zeurus");
-  } else if cfg!(unix) {
-      tauri::Builder::default()
-          .invoke_handler(tauri::generate_handler![get_input, show_main_window])
-          .run(tauri::generate_context!())
-          .expect("error while running zeurus");
-  } else if cfg!(target_os = "macos") {
-      tauri::Builder::default()
-          .invoke_handler(tauri::generate_handler![get_input, show_main_window])
-          .run(tauri::generate_context!())
-          .expect("error while running zeurus");
-  }
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_input, show_main_window])
+        .setup(|app| {
+            let window = app.get_window(&"main").unwrap();
+            window_shadows::set_shadow(&window, true).expect("Unsupported platform!");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running zeurus");
 }
+
+#[cfg(target_os = "macos")]
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_input, show_main_window])
+        .run(tauri::generate_context!())
+        .expect("error while running zeurus");
+}
+
+#[cfg(target_os = "linux")]
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_input, show_main_window])
+        .run(tauri::generate_context!())
+        .expect("error while running zeurus");
+}
+
 
 #[tauri::command]
 fn get_input() -> Vec<String> {
-  let args = env::args().collect();
-  args
+    let args = env::args().collect();
+    args
 }
 #[tauri::command]
 async fn show_main_window(window: tauri::Window) {
-    window.get_window("main").unwrap().show().unwrap(); 
+    window.get_window("main").unwrap().show().unwrap();
 }
